a339220df082100231d0919d1fadbe5239914257
Icaro Justino <icarojustinom@hotmail.com>
2024-05-19 18:16:18 -0300

    pagina 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,10 @@
 export default function NotFoundPage() {
     return (
         <>
-            <div className="">
-                404 page
+            <div className="w-screen h-screen flex ">
+                <div className="m-auto bg-login lg:p-32 md:p-32 p-16 w-fit shadow-simple rounded-2xl font">
+                    Nothing here but chickens
+                </div>
             </div>
         </>
     )
